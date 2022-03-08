d07c2e44b2adb286b461d1ff9aa22313a9a7897c
Vikram <59924856+justVikram@users.noreply.github.com>
2022-03-08 16:43:17 +0530

    add code to preprocess video

---
+++ preprocess.py
@@ -0,0 +1,64 @@
+import cv2 as cv
+import os
+
+
+def preprocess_vid(source_path, output_path):
+    count = 0
+    for vid in os.listdir(source_path):
+        if vid.endswith('.mp4'):
+            count = count + 1
+            vid = os.path.join(source_path, vid)
+            split_video(vid, output_path, count)
+
+
+# Function to split video into chunks of duration = 2 seconds and save them in a folder
+def split_video(video_path, output_path, count):
+    # Code to get duration of input video
+    cap = cv.VideoCapture(video_path)
+    duration = int(cap.get(cv.CAP_PROP_FRAME_COUNT) / cap.get(cv.CAP_PROP_FPS))
+    cap.release()
+
+    new_output_path = os.path.join(output_path, f'video_{str(count)}')
+    os.mkdir(new_output_path)
+    os.mkdir(os.path.join(new_output_path, 'segments'))
+
+    seek = 0
+    while duration - seek >= 2:
+        start = seek
+        end = seek + 2
+        os.system(
+            f'ffmpeg -i {video_path} -ss 00:00:0{start} -t 00:00:02 -c:v libx264 -c:a copy {new_output_path}/segments/segment{start}-{end}.mp4')
+        seek = seek + 2
+
+    # Get frames for each segment
+    segment_count = 0
+    for segment in os.listdir(os.path.join(new_output_path, 'segments')):
+        if segment.endswith('.mp4'):
+            segment_count = segment_count + 1
+            frames_dir = os.path.join(new_output_path, f'frames_for_segment_{segment_count}')
+            os.mkdir(frames_dir)
+            get_frames(os.path.join(f'{new_output_path}/segments', segment), frames_dir)
+
+
+# Function to get frames from video into frames
+def get_frames(video_path, output_path):
+    cap = cv.VideoCapture(video_path)
+    frame_count = 0
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            frame_count = frame_count + 1
+            cv.imwrite(os.path.join(os.path.dirname(output_path), f'frame_{frame_count}.jpg'), frame)
+        else:
+            break
+
+    cap.release()
+
+
+if __name__ == '__main__':
+    # Split video into frames
+    # op = get_frames('./data/input/vid.mp4', 0.1)
+
+    source_path = '/Users/avikram/Projects/av-emotion-recognition/data/input'
+    output_path = '/Users/avikram/Projects/av-emotion-recognition/data/output'
+    preprocess_vid(source_path, output_path)
