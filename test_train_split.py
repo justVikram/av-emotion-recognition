9bfc184ff71bc3065aa181ff08dd3f2c67c29ed8
Vikram <59924856+justVikram@users.noreply.github.com>
2022-03-15 22:59:55 +0530

    add code to create text files containing test-train file names

--- test_train_split.py
+++ test_train_split.py
@@ -1,17 +1,15 @@
-import shutil
-
+import random
 from sklearn.model_selection import train_test_split
 import os
 from distutils.dir_util import copy_tree
 
 
-def test_train_split(path: os.path) -> None:
+def segregate_emotions(path: os.path) -> None:
     label = []
     for folder_name in sorted(os.listdir(path)):
         if folder_name.startswith('.') is False:
             identifiers = folder_name.split('_')
-            if identifiers[2] == 'happy' or identifiers[2] == 'sad' \
-                    or identifiers[2] == 'angry' or identifiers[2] == 'neutral':
+            if identifiers[2] == 'happy' or identifiers[2] == 'sad':
                 label.append(identifiers[2])
                 copy_tree(os.path.join(path, folder_name), os.path.join(path, '../dataset', folder_name))
 
@@ -19,5 +17,40 @@ def test_train_split(path: os.path) -> None:
         os.listdir(os.path.join(path, '../dataset')), label, test_size=0.2, random_state=42)
 
 
-if __name__ == '__main__':
-    test_train_split('/Users/avikram/Projects/av-emotion-recognition/expt')
+def segregate_into_train_test(path: os.path) -> None:
+    os.mkdir(os.path.join(path, '../train'))
+    os.mkdir(os.path.join(path, '../test'))
+
+    unique_actors = set()
+
+    for folder in os.listdir(path):
+        if folder.startswith('.') is False:
+            identifiers = folder.split('_')
+            unique_actors.add(identifiers[0])
+
+    print(unique_actors)
+
+    for actor in unique_actors:
+        prob = random.random()
+        for folder in os.listdir(path):
+            if folder.startswith(actor):
+                if prob <= 0.8:
+                    copy_tree(os.path.join(path, folder), os.path.join(path, '../train', folder))
+                else:
+                    copy_tree(os.path.join(path, folder), os.path.join(path, '../test', folder))
+
+
+def convert_train_to_txt(path):
+    files = os.listdir(path)
+    with open('/home/sg/AV/train.txt', 'w') as txtfile:
+        for file in files:
+            txtfile.write(file)
+            txtfile.write('\n')
+
+
+def convert_test_to_txt(path):
+    files = os.listdir(path)
+    with open('/home/sg/AV/val.txt', 'w') as txtfile:
+        for file in files:
+            txtfile.write(file)
+            txtfile.write('\n')
